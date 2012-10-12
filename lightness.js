5713b4ff8388964e117b567c16ff8b5be5b361f2
David Jensen <david.lgj@gmail.com>
2012-10-12 07:05:13 +0200

    Implemented pixelate via scaling in lib, much better performance and more stable results

--- lightness.js
+++ lightness.js
@@ -80,8 +80,8 @@
   
     //figure out the grid step size
     var result = [];
-    var dx = Math.floor(canvas.width/gridx); //we're going to miss a pixel here and there if width is not divisable by gridsize, but that's ok
-    var dy = Math.floor(canvas.height/gridy);
+    var dx = Math.round(canvas.width/gridx); //we're going to miss a pixel here and there if width is not divisable by gridsize, but that's ok
+    var dy = Math.round(canvas.height/gridy);
     var y = 0;
     var x = 0;
     
@@ -104,11 +104,57 @@
   };
   
   
+  //pixelate by scaling
+  var pixelate2 = function(image,gridx,gridy) {
+    var canvas =  document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+    
+    canvas.width  = gridx;
+    canvas.height = gridy;
+    
+    //draw image to scale of grid
+    ctx.drawImage(image,0,0,gridx,gridy);
+    
+    //getImage data from that
+    var data = ctx.getImageData(0,0,gridx,gridy).data;
+    
+    //make a list of blocks
+    var result = [];
+    
+    var w  = image.width/gridx;
+    var h = image.height/gridy;
+    var width  = Math.round(w);
+    var height = Math.round(h);
+    
+    for (var i=0;i<data.length; i += 4) {
+      var p = i/4;
+      result.push({
+        x: Math.round((p % gridx)*w),
+        y: Math.round(Math.floor(p/gridx) * h),
+        width: width,
+        height: height,
+        color: {
+          r: data[i],
+          g: data[i+1],
+          b: data[i+2]
+        }
+      });
+    }
+    console.log(result)
+    return result;
+  };
+  
+  
   /**
    * Figure out lightness by transforming pixelated image data to hsl via TinyColor
    */ 
-  var lightness = function(image,gridx,gridy) {
-    var pixels = pixelate(image,gridx,gridy);
+  var lightness = function(image,gridx,gridy,method) {
+    
+    if (method === 'average') {
+      var pixels = pixelate(image,gridx,gridy);
+    } else {
+      var pixels = pixelate2(image,gridx,gridy);
+    }
     
     for (var i=0; i<pixels.length; i++) {
       var p = pixels[i];
